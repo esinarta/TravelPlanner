85da9d9ffbbf86f4ed9d9e9c8d18d572d2bfbc84
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2019-06-14 19:23:17 -0700

    added bcrypt

--- models/user.js
+++ models/user.js
@@ -2,11 +2,24 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   email: {
-    type: String,
+    type: String
   },
   password: {
-    type: String,
+    type: String
+  },
+  first_name: {
+    type: String
+  },
+  last_name: {
+    type: String
   }
+});
+
+userSchema.pre('save', function(next) {
+  this.first_name = this.first_name.toLowerCase();
+  this.last_name = this.last_name.toLowerCase();
+  this.email = this.email.toLowerCase();
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const models = require('../models/');
 const mongoose = require("mongoose");
+const Bcrypt = require('bcryptjs');
 
 mongoose.connect('mongodb://localhost:27017/local', {
   useNewUrlParser: true
@@ -9,12 +10,39 @@ mongoose.connect('mongodb://localhost:27017/local', {
 
 const User = models.User;
 
-router.post('/', (req, res) => {
+//Register
+router.post('/register', (req, res) => {
+  req.body.password = Bcrypt.hashSync(req.body.password, 10);
   let user = new models.User(req.body);
-  user.save();
-})
+  user.save((err) => {
+    console.log(err);
+    if (err) {
+      res.send(err);
+    } else {
+      //req.session.currentUser = user;
+      res.redirect('/');
+    }
+  });
+});
 
-/* get users */
+//Login
+router.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email.toLowerCase() }, (err, user) => {
+    if (err) {
+      console.error(err);
+      res.redirect('/login');
+    }
+    //Validate password with Bcrypt
+    if(!user || !Bcrypt.compareSync(req.body.password, user.password)) {
+      res.redirect('/login');
+    } else {
+      //req.session.currentUser = user;
+      res.redirect('/');
+    }
+  });
+});
+
+//Get users
 router.get('/', function(req, res) {
   User.find( (err, user) => {
     res.json(user);
